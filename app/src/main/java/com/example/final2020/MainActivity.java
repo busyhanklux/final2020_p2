c2a8a531a8c01c44954307d7bce5a3db4ffb31ea
Fangan998 <lgeorge241@gmail.com>
2021-01-09 18:22:28 +0800

    新增了選項 -可查詢台灣天氣 介面調整 -不再是亂七八糟的 -linear_layout add -sp_city -string-array   -23 item renew -layout  -txt

--- app/src/main/java/com/example/final2020/MainActivity.java
+++ app/src/main/java/com/example/final2020/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.final2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -24,30 +27,32 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView t1_temp,t2_city,t3_description,t4_date;
+    TextView t_temp,t_city,t_description,t_date;
     Button b1_decide;
-    EditText e1_target;
+    Spinner e1_target;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t1_temp = (TextView)findViewById(R.id.t1);
-        t2_city = (TextView)findViewById(R.id.t2);
-        t3_description = (TextView)findViewById(R.id.t3);
-        t4_date = (TextView)findViewById(R.id.t4);
+        t_temp = (TextView)findViewById(R.id.t1);
+        t_city = (TextView)findViewById(R.id.t3);
+        t_description = (TextView)findViewById(R.id.t4);
+        t_date = (TextView)findViewById(R.id.t2);
         
-        e1_target = (EditText)findViewById(R.id.editTextTextPersonName);
-        find_weather(e1_target);
+        e1_target = (Spinner)findViewById(R.id.sp_city);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this,R.array.tw_cities,android.R.layout.simple_spinner_dropdown_item);
+        e1_target.setAdapter(adapter);
 
+        find_weather(e1_target);
     }
 
     public void find_weather(View view) {
 
         b1_decide = (Button)findViewById(R.id.button);
-        String url = "https://api.openweathermap.org/data/2.5/weather?q="+ e1_target.getText() +"&appid=a3687731d05eb6f4484ea5b83b087c09";
-
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+ e1_target.getSelectedItem() +"&appid=a3687731d05eb6f4484ea5b83b087c09";
+        Log.d("nameOfChty", String.valueOf(e1_target.getSelectedItem()));
         JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET,url,null, new Response.Listener<JSONObject>() {
 
             public void onResponse(JSONObject response){
@@ -60,21 +65,21 @@ public class MainActivity extends AppCompatActivity {
                     String city            = response.getString("name");
 
                     //t1_temp.setText(temp);
-                    t2_city.setText("city："+city);//城市
-                    t3_description.setText("weather："+description);//天氣概況
+                    t_city.setText(city);//城市
+                    t_description.setText(description);//天氣概況
 
                     Calendar calendar     = Calendar.getInstance();
                     SimpleDateFormat sdf  = new SimpleDateFormat("EEEE-MM-dd");//星期-月-日
                     String formatted_date = sdf.format(calendar.getTime());
 
-                    t4_date.setText("today："+formatted_date);
+                    t_date.setText(formatted_date);
 
                     double temp_int = Double.parseDouble(temp);
                     //double centi    = (temp_int - 32) / 1.8000;
                     double centi    = temp_int - 273.15;//絕對溫度 轉 攝氏度
                     centi           = Math.round(centi);
                     int i           = (int)centi;
-                    t1_temp.setText("temperature："+String.valueOf(i));
+                    t_temp.setText(String.valueOf(i)+" °C");
 
                 }catch (JSONException e) {
                     e.printStackTrace();
